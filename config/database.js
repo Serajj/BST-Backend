d8d2e120df068ef3ddbcc1c216ed1be27d616b40
Seraj Alam <rajalam2372@gmail.com>
2021-04-20 12:48:03 +0530

    Merge br

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const pool = createPool({
     port: 3306,
     host: "localhost",
     user: "root",
-    password: "Seraj",
+    password: "",
     database: "mycollegeproject",
     connectionLimit: 10
 });
